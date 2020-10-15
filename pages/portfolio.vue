050bd46e3bba425b4dfa0306a7fd1940d4f64714
hariswb <hariswb@gmail.com>
2020-10-15 20:57:54 +0700

    Change font, fix layout

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
   <Nav page="Portfolio"/>
-  <ul class="portfolio">
-    <li v-for="item in portfolios" :key="item.id">
+  <div class="portfolio">
+    <div v-for="item in portfolios" :key="item.id">
       <div class="title" @click="handleSelect(item.id)">
         <button><h3>{{item.title}}</h3></button>
       </div>
-      <div class="content" v-show="showContent(item.id)">
+      <div class="content" :class="showContent(item.id)">
         <a :href="item.demoUrl" target="_blank" v-show="showLink(item.demoUrl)">
           <MyButton name="Live" height="30px" width="70px" radius="30px"/>
         </a>
@@ -15,13 +15,14 @@
         </a>
         <p>{{item.article}}</p>
       </div>
-    </li>
-  </ul>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 import Portfolios from "../static/data/portfolios.json"
+
 export default {
   data(){
     return {
@@ -34,7 +35,7 @@ export default {
       this.selected = id
     },
     showContent(id){
-      return id==this.selected?true:false
+      return id==this.selected?"show":"show"
     },
     showLink(url){
       return url.length>0?true:false
@@ -45,10 +46,13 @@ export default {
 
 <style scoped>
   .portfolio {
-    list-style-type: none;
     margin: 0;
     margin-top: 100px;
     padding: 0;
+    display:flex;
+    flex-direction: column;
+    margin:auto;
+    max-width: 600px;
   }
   .title button{
     border:none;
@@ -57,31 +61,34 @@ export default {
   }
 
   h3{
-    font-size: 1.2rem;
-    margin:10px 0 10px 5px;
+    font-size: 1.4rem;
+    margin:20px 0 10px 0px;
     color: rgb(54, 54, 54);
   }
-  li{
-    max-width: 400px; 
-  } 
-  .content{
-    padding: 15px;
-    /* background-color: aquamarine; */
-    /* border: 0px solid black; */
-    /* border-radius: 10px 10px 10px 10px;
-    box-shadow: 
-            inset 1px 2px 3px rgba(0, 0, 0, 0.055),
-            inset -1px -2px 3px rgba(255, 255, 255, 0.856); */
-  }
 
+  .content{
+    overflow: hidden;
+  }
+  .show{
+    transition:all ease-in .5s;
+    max-height: 600px;
+    opacity: 1;
+  }
+  .hide{
+    transition:all ease-out .5s;
+    max-height:0px;
+    opacity: 0;
+  }
+  
   p{
-    text-align: left;
-    font-size: 1rem;
+    text-align: justify;
+    text-justify: auto;
+    font-size: 1.05rem;
     color: rgb(71, 71, 71);
   }
-  @media only screen and (min-width: 600px) {
-    .portfolio {
-      margin-left: 25%;
+  @media only screen and (max-width: 400px) {
+    p{
+      text-align: left;
     }
   } 
 </style>
